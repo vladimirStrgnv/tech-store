e8a100c81ead5167ec71b19df81406a513358eec
unknown <vstroganov.reon@gmail.com>
2023-09-29 21:05:52 +0300

    wip: main page

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -5,15 +5,30 @@ const MainPage = () => {
     <section className="advantages">
       <div className="advantages__info">
         <div className="advantages__service-info">
-          <h2 className="advantages__service-info-title">Установка и продажа систем видеонаблюдения</h2>
-          <p className="advantages__service-info-description">Понятным языком объясняем технические термины</p>
+          <ul className="advantages__tags-list">
+            <li className="advantages__tag">Телефоны</li>
+            <li className="advantages__tag">Ростов-на-Дону</li>
+          </ul>
+          <h2 className="advantages__service-info-title">
+            Установка и продажа систем видеонаблюдения
+          </h2>
+          <p className="advantages__service-info-description">
+            Понятным языком объясняем технические термины
+          </p>
         </div>
         <div className="advantages__catalog_info">
           <h2 className="advantages__catalog-info-title">ARSHIN TECH</h2>
-          <p className="advantages__catalog-info-description">Объединение профессионалов, с большим опытом работы в сфере систем видеонаблюдения, контроля доступа и видеодомофонии.</p>
-          <button className="advantages__catalog-info-btn">Оставить заявку</button>
+          <p className="advantages__catalog-info-description">
+            Объединение профессионалов, с большим опытом работы в сфере систем
+            видеонаблюдения, контроля доступа и видеодомофонии.
+          </p>
+          <button className="advantages__catalog-info-btn">
+            Оставить заявку
+          </button>
         </div>
+
       </div>
+      <div className="advantages__img"></div>
     </section>
   );
 };
